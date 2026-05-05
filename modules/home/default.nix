eca4d20b81e9e8d5a687e7b8dd9355fd27cbb82a
Isaac Dobbins <isaac.dobbins@icloud.com>
2026-05-05 15:01:32 -0600

    Disable Home Manager manual generation

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -12,5 +12,10 @@
   ];
 
   programs.home-manager.enable = true;
+
+  manual.manpages.enable = false;
+  manual.html.enable = false;
+  manual.json.enable = false;
+
   home.stateVersion = "24.11";
 }
